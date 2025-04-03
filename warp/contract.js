d858a44c30291c04ed037258856b07258a15a302
Wali <humaidwali20@gmail.com>
2025-04-03 19:39:25 +0530

    i dont know

--- warp/contract.js
+++ warp/contract.js
@@ -11,7 +11,16 @@ export function handle(state , action){
         state.posts = posts
         state.postCount = state.postCount || 0
         state.postCount += 1
-        
+    }
+    if(action.input.function === "editPost" && action.caller == state.author){
+        const posts = state.posts || []
+        const postToUpdate = posts[action.input.post.id]
+        posts[action.input.post.id] = action.input.post
+        state.posts = posts
+    }
+
+    if(action.input.function === "deletePost" && action.caller == state.author){
+
     }
 
     return (
